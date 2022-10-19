25e93c2a9f7c91fb656ae2b800db9a55e5540e06
hideki-okawa <h.okawa0303@gmail.com>
2022-10-19 19:30:43 +0900

    format

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,10 +1,10 @@
 module Api
-    class QuestionsController < ApplicationController
-      def index
-        questions = Question.all
-        render json: {
-          questions: questions
-        }, status: :ok
-      end
+  class QuestionsController < ApplicationController
+    def index
+      questions = Question.all
+      render json: {
+        questions: questions
+      }, status: :ok
     end
+  end
 end
