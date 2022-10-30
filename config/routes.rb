97efb3ea93425252c37b94af55f1f7f4470466c8
hideki-okawa <h.okawa0303@gmail.com>
2022-10-30 17:09:29 +0900

    認証APIの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   namespace :api do
     resources :questions, only: %i[index show create]
     resources :questions do
       resources :answers, only: %i[index]
+    end
+
+    mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+      registrations: 'api/auth/registrations'
+    }
+
+    namespace :auth do
+      resources :sessions, only: %i[index]
     end
   end
 end
